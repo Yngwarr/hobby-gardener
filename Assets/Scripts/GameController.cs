0162f189772288999a5108ff38b881ad4537628f
Igor Kislitsyn <k.yngvarr@gmail.com>
2023-01-08 19:17:53 +0700

    populated the world with soil

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    const int W = 8;
+    const int H = 6;
+    const int Step = 4;
+    
+    [SerializeField] GameObject soilPrefab;
+    
+    void Start() {
+        for (var x = 0; x < W; ++x) {
+            for (var y = 0; y < H; ++y) {
+                Instantiate(soilPrefab, new Vector3(x * Step, 0, y * Step), Quaternion.identity);
+            }
+        }
+    }
+}
