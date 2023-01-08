0cf5a5744a136edde45d6f35d54418a555c9e789
Igor Kislitsyn <k.yngvarr@gmail.com>
2023-01-09 02:33:41 +0700

    all plants added

--- Assets/Scripts/GardenBed.cs
+++ Assets/Scripts/GardenBed.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
+public class GardenBed : MonoBehaviour
 {
     const int W = 8;
     const int H = 6;
@@ -14,7 +14,11 @@ public class GameController : MonoBehaviour
     void Start() {
         for (var x = 0; x < W; ++x) {
             for (var y = 0; y < H; ++y) {
-                Instantiate(soilPrefab, new Vector3(x * Step, 0, y * Step), Quaternion.identity);
+                Instantiate(soilPrefab,
+                    new Vector3(x * Step, 0, y * Step),
+                    Quaternion.identity,
+                    transform
+                );
             }
         }
     }
---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public void Tick() {}
+    public void Harvest() {}
+}
